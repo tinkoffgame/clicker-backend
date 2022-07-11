a62b2b63de28c5086188547d8a4ac07ad4dd7b7d
TiusTofrie <golem238@mail.ru>
2022-07-11 09:45:20 +0300

    updated server.py

--- server.py
+++ server.py
@@ -13,52 +13,51 @@ DATA_LIST = [
     'hash',
     'chat_id'
 ]
-API = 'https://api.tinkoffgame.ml/'  # https://api.tinkoffgame.ml/
+API = 'https://api.tinkoffgame.ml/'
 VALIDATE_API = ''
-SCORE_API = 'v1/users/'
+SCORE_API = 'api/v1/users/'
 
+@app.route('api/v1/auth/', methods=['POST'])
+def auth():
+	data = {}
+	for data_name in DATA_LIST:
+		data[data_name] = flask.request.args.get(data_name)
+	if data['id']:
+		# TODO: make validation
+		flask.session['data'] = data
 
-#@app.route('/')
-#def index():
-#    if 'data' in flask.session:
-#        return flask.render_template(
-#            'index.html',
-#            chat_id=flask.session['data']['chat_id'],
-#            user_id=flask.session['data']['id'] + 'b'
-#        )
-#    else:
-#        data = {}
-#        for data_name in DATA_LIST:
-#            data[data_name] = flask.request.args.get(data_name)
-#        if data['id']:
-#            # TODO: make validation
-#            # if api_method(VALIDATE_API, {'data': data})
-#
-#            flask.session['data'] = data
-#            return flask.render_template(
-#                'index.html',
-#                chat_id=flask.session['data']['chat_id'],
-#                user_id=flask.session['data']['id']
-#            )
-#        else:
-#            return 'Auth error'
-@app.route('/score/', methods=['GET','POST','PUT'])
-def score():
+	
+
+@app.route('api/v1/users/', methods=['GET','POST','PUT'])
+def users():
 	if flask.request.method == 'PUT':
 		score = flask.request.form.get('score')
-		user_id = flask.request.form.get('user_id')
-		chat_id = flask.request.form.get('chat_id')
-		resp = requests.put(API+SCORE_API+user_id, {'score': score, 'game_id': GAME_ID})
+		telegram_id = flask.session['data']['id']
+
+		# telegram_id = flask.request.form.get('telegram_id')
+
+		resp = requests.put(API+SCORE_API, {'game_id': GAME_ID, 'telegram_id': telegram_id, 'score': score})
 		return resp.content
+
 	if flask.request.method == 'POST':
-		user_id = flask.request.form.get('user_id')
-		chat_id = flask.request.form.get('chat_id')
-		resp = requests.post(API+SCORE_API, {'game_id': GAME_ID, 'user_id': user_id, 'chat_id': chat_id})
+		telegram_id = flask.session['data']['id']
+		chat_id = flask.session['data']['chat_id']
+		name = flask.session['data']['first_name'] + ' ' + flask.session['data']['last_name']
+
+		# telegram_id = flask.request.form.get('telegram_id')
+		# chat_id = flask.request.form.get('chat_id')
+		# first_name = flask.request.form.get('first_name') + ' ' + flask.request.form.get('last_name')
+		
+		resp = requests.post(API+SCORE_API, {'game_id': GAME_ID, 'telegram_id': telegram_id, 'chat_id': chat_id, 'name': name})
 		return resp.content
+
 	if flask.request.method == 'GET':
-		user_id = flask.request.form.get('user_id')
-		chat_id = flask.request.form.get('chat_id')
-		resp = requests.get(API+SCORE_API, {'game_id': GAME_ID, 'user_id': user_id, 'chat_id': chat_id})
+		telegram_id = flask.session['data']['id']
+		chat_id = flask.session['data']['chat_id']
+		# telegram_id = flask.request.form.get('telegram_id')
+		# chat_id = flask.request.form.get('chat_id')
+
+		resp = requests.get(API+SCORE_API, {'game_id': GAME_ID, 'telegram_id': telegram_id, 'chat_id': chat_id})
 		return resp.content
 
 
