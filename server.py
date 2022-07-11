5b2c1fe9221fd8782705f29e16d6d2b11ac422f4
TiusTofrie <golem238@mail.ru>
2022-07-11 17:12:43 +0300

    1

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from crypt import methods
 import flask
 import requests
 import secrets2
+import json
+from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 GAME_ID = 1
@@ -16,39 +17,45 @@ DATA_LIST = [
     'chat_id'
 ]
 # API = 'https://api.tinkoffgame.ml/'
-API = 'http://906f-188-170-214-41.ngrok.io/'
+API = 'http://064e-188-170-214-41.ngrok.io/'
 SCORE_API = 'api/v1/users/'
 
 
 @app.route("/index", methods= ['POST', 'GET'])
+@cross_origin()
 def index():
 	return "hello"
 
-@app.route('/api/v1/auth/', methods=['POST'])
+@app.route('/api/v1/auth/', methods=['POST', 'OPTIONS'])
+@cross_origin()
 def auth():
-	data = {}
-	json1 = flask.request.get_json()
-	# print(json1)
-	# for data_name in DATA_LIST:
-	# 	data[data_name] = json1.get(data_name)
-	# if data['id']:
-	# 	# TODO: make validation
-		
-	# 	flask.session['data'] = data
+	if flask.request.method == 'OPTIONS':
+		return 'OK'
+	if flask.request.method == "POST":
+		data = {}
+		print(flask.request.headers)
+		json1 = flask.request.data
+		json1 = json.loads(json1)
+		for data_name in DATA_LIST:
+			data[data_name] = json1.get(data_name)
+		if data['id']:
+			# TODO: make validation
+			
+			flask.session['data'] = data
 
-	# 	telegram_id = flask.session['data']['id']
-	# 	chat_id = flask.session['data']['chat_id']
-	# 	name = flask.session['data']['first_name'] + ' ' + flask.session['data']['last_name']
+			telegram_id = flask.session['data']['id']
+			chat_id = flask.session['data']['chat_id']
+			name = flask.session['data']['first_name'] + ' ' + flask.session['data']['last_name']
 
-	# 	resp = requests.post(API+SCORE_API, json={'game_id': GAME_ID, 'telegram_id': telegram_id, 'chat_id': chat_id, 'name': name})
-	# 	return resp.content
-	# else:
-	# 	return str(400)
-	return str(200)
-
+			resp = requests.post(API+SCORE_API, json={'game_id': GAME_ID, 'telegram_id': telegram_id, 'chat_id': chat_id, 'name': name}).json()
+			print(resp)
+			return resp
+		else:
+			return str(400)
 	
 
 @app.route('/api/v1/users/', methods=['GET','POST','PUT'])
+@cross_origin()
 def users():
 	if flask.request.method == 'PUT':
 		score = flask.request.get_json().get('score')
