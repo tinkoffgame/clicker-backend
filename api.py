5fdc539b1cd212e4295c14936c3095f68beff844
Andrey <Kizhinov05@gmail.com>
2022-07-17 14:23:52 +0300

    Clicker defender

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import requests
 import uvicorn
-from fastapi import FastAPI, Response, Cookie
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -8,57 +8,14 @@ API = "https://api.tinkoffgame.ml"
 GAME_ID = 1
 SCORE_API = "/api/v1/users"
 app = FastAPI()
-origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT"],
     allow_headers=["*"],
 )
-
-
-class AuthBase(BaseModel):
-    telegram_id: str
-    first_name: str
-    last_name: str
-    chat_id: str
-
-
-@app.post("/api/v1/auth")
-async def authorization(model_in: AuthBase):
-    name = model_in.first_name + ' ' + model_in.last_name
-    resp = requests.post(API + SCORE_API, json={'game_id': GAME_ID,
-                                                'telegram_id': model_in.telegram_id,
-                                                'chat_id': model_in.chat_id,
-                                                'name': name}).json()
-    print(resp)
-    return resp
-
-
-class UpdateUser(BaseModel):
-    score: str
-    telegram_id: str
-
-
-@app.put("/api/v1/users")
-def update_user(model_in: UpdateUser):
-    resp_json = requests.put(API + SCORE_API, json={'game_id': GAME_ID,
-                                                    'telegram_id': model_in.telegram_id,
-                                                    'score': int(model_in.score)}).json()
-    print(resp_json)
-    return resp_json
-
-
-@app.post("/api/v1/users")
-def create_user(model_in: AuthBase):
-    name = f"{model_in.first_name} {model_in.last_name}"
-    resp_json = requests.post(API + SCORE_API, json={'game_id': GAME_ID,
-                                                     'telegram_id': model_in.telegram_id,
-                                                     'chat_id': model_in.chat_id,
-                                                     'name': name}).json()
-    print(resp_json)
-    return resp_json
+score = dict()
 
 
 class GetUser(BaseModel):
@@ -66,11 +23,54 @@ class GetUser(BaseModel):
     chat_id: str
 
 
+class UpdateTable(BaseModel):
+    telegram_id: str
+    score: int
+
+
+class AuthBase(BaseModel):
+    telegram_id: int
+    first_name: str
+    last_name: str
+    chat_id: int
+
+
+@app.post("/api/v1/auth")
+async def authorization(model_in: AuthBase):
+    name = f"{model_in.first_name} {model_in.last_name}"
+    resp_json = requests.post(API + SCORE_API,
+                              json={'game_id': GAME_ID,
+                                    'telegram_id': model_in.telegram_id,
+                                    'chat_id': model_in.chat_id,
+                                    'name': name}).json()
+    score[model_in.telegram_id] = 0
+    return resp_json
+
+
+@app.put("/api/v1/score")
+def update_user(model_in: UpdateTable):
+    if model_in.score <= 60:
+        score[model_in.telegram_id] += model_in.score
+    return {"score_now": score[model_in.telegram_id]}
+
+
+@app.put("/api/v1/users")
+def update_user(model_in: UpdateTable):
+    resp_json = requests.put(API + SCORE_API,
+                             json={'game_id': GAME_ID,
+                                   'telegram_id': model_in.telegram_id,
+                                   'score': int(
+                                       score[model_in.telegram_id])}).json()
+    score.pop(model_in.telegram_id, None)
+    return resp_json
+
+
 @app.get('/api/v1/users')
-def get_user(model_in: GetUser):
-    resp_json = requests.get(API + SCORE_API, params={'game_id': GAME_ID,
-                                                      'telegram_id': model_in.telegram_id,
-                                                      'chat_id': model_in.chat_id}).json()
+def get_user(telegram_id: int, chat_id: int):
+    resp_json = requests.get(API + SCORE_API,
+                             params={'game_id': GAME_ID,
+                                     'telegram_id': telegram_id,
+                                     'chat_id': chat_id}).json()
     return resp_json
 
 
