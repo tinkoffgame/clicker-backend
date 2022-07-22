207bd5111cce19f730777eee6c17a8ea552ff66c
maksim-mshp <maksim-mshp@ya.ru>
2022-07-22 23:36:01 +0300

    бан за частые клики

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import datetime
 
 API = "https://api.tinkoffgame.ml"
 GAME_ID = 1
@@ -18,6 +19,7 @@ app.add_middleware(
 )
 score = dict()
 
+requests_list = dict()
 
 class GetUser(BaseModel):
     telegram_id: int
@@ -40,6 +42,26 @@ class AuthBase(BaseModel):
 async def authorization(model_in: AuthBase, telegram_id: int):
     if model_in.telegram_id != telegram_id:
         raise HTTPException(status_code=401)
+    
+    if (not telegram_id in requests_list):
+        requests_list[telegram_id] = {
+            'last_request': datetime.datetime.now(),
+            'ban': False
+        }
+    else:
+        requests_list[telegram_id]['last_request'] = datetime.datetime.now()
+        ban = requests_list[telegram_id]['ban'] 
+        if (ban != False):
+            t = ban + datetime.timedelta(minutes=10)
+            if (datetime.datetime.now() > t):
+                requests_list[telegram_id]['ban'] = False
+            else:
+                score[telegram_id] = 0
+                t += datetime.timedelta(hours=3)
+                raise HTTPException(429, detail={
+                    'unban': t.strftime('%d.%m.%Y %H:%M:%S')
+                })
+    
     name = f"{model_in.first_name} {model_in.last_name}"
     requests.post(API + SCORE_API, json={'game_id': GAME_ID,
                                          'telegram_id': model_in.telegram_id,
@@ -57,6 +79,40 @@ async def authorization(model_in: AuthBase, telegram_id: int):
 def update_user(model_in: UpdateUser, telegram_id: int):
     if model_in.telegram_id != telegram_id:
         raise HTTPException(status_code=401)
+    
+    print(requests_list)
+    
+    if (not telegram_id in requests_list):
+        requests_list[telegram_id] = {
+            'last_request': datetime.datetime.now(),
+            'ban': False
+        }
+    else:
+        lr = requests_list[telegram_id]['last_request']    
+        if (lr + datetime.timedelta(seconds=2) > datetime.datetime.now()):
+            ban = datetime.datetime.now()
+            requests_list[telegram_id]['ban'] = ban
+            t = ban + datetime.timedelta(minutes=10)
+            score[telegram_id] = 0
+            t += datetime.timedelta(hours=3)
+            raise HTTPException(429, detail={
+                    'unban': t.strftime('%d.%m.%Y %H:%M:%S')
+                })
+            
+        requests_list[telegram_id]['last_request'] = datetime.datetime.now()
+        ban = requests_list[telegram_id]['ban'] 
+        if (ban != False):
+            t = ban + datetime.timedelta(minutes=10)
+            if (datetime.datetime.now() > t):
+                requests_list[telegram_id]['ban'] = False
+            else:
+                score[telegram_id] = 0
+                t += datetime.timedelta(hours=3)
+                raise HTTPException(429, detail={
+                    'unban': t.strftime('%d.%m.%Y %H:%M:%S')
+                })
+    
+                
     if model_in.score <= 60:
         score[model_in.telegram_id] += model_in.score
     return {"score_now": score[model_in.telegram_id]}
@@ -66,6 +122,26 @@ def update_user(model_in: UpdateUser, telegram_id: int):
 def update_user(model_in: UpdateUser, telegram_id: int):
     if model_in.telegram_id != telegram_id:
         raise HTTPException(status_code=401)
+    
+    if (not telegram_id in requests_list):
+        requests_list[telegram_id] = {
+            'last_request': datetime.datetime.now(),
+            'ban': False
+        }
+    else:
+        requests_list[telegram_id]['last_request'] = datetime.datetime.now()
+        ban = requests_list[telegram_id]['ban'] 
+        if (ban != False):
+            t = ban + datetime.timedelta(minutes=10)
+            if (datetime.datetime.now() > t):
+                requests_list[telegram_id]['ban'] = False
+            else:
+                score[telegram_id] = 0
+                t += datetime.timedelta(hours=3)
+                raise HTTPException(429, detail={
+                    'unban': t.strftime('%d.%m.%Y %H:%M:%S')
+                })
+                
     if model_in.score <= 60:
         temp_score = score[model_in.telegram_id] + model_in.score
         score.pop(model_in.telegram_id, None)
@@ -77,4 +153,4 @@ def update_user(model_in: UpdateUser, telegram_id: int):
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=8080, reload=False)
+    uvicorn.run("api:app", host="127.0.0.1", port=8080, reload=True)
